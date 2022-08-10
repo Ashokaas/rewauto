3ebfc9b40da2026b43deb636b2c222989d870b30
RedGaming98 <51047087+RedGaming98@users.noreply.github.com>
2022-08-10 18:14:23 +0200

    Quasi enlevé toute utilisation de pyautogui sauf Ceci ou Cela car je ne sais pas comment ca marche.

---
+++ login_ui.py
@@ -0,0 +1,70 @@
+import tkinter as tk
+from os.path import expanduser
+
+
+def login_ui():
+    global output
+
+    home_folder = expanduser("~").replace("\\", "/")
+
+    save = open(home_folder + "/rewauto_data.txt", "r")
+    data = save.readlines()
+    data += ["" for _ in range(2 - len(data))]
+
+    def done():
+        savew = open(home_folder + "/rewauto_data.txt", "w")
+        data[0] = remember_values[0].get() * email_input.get()
+        data[1] = remember_values[1].get() * pass_input.get()
+        data.insert(1, "\n")
+
+        savew.writelines(data)
+        global output
+        output = (email_input.get(), pass_input.get())
+        print(output)
+        root.destroy()
+        return
+
+    root = tk.Tk()
+    root.geometry("500x500")
+    root.title("Login")
+
+    tk.Label(root, text="Rewauto",
+             font=('bold', 20), wraplength=450).pack()
+    tk.Label(root, text="Please enter the Email and Password of your Microsoft Rewards account",
+             font=('bold', 10), wraplength=450).pack()
+
+    login_frame = tk.Frame(root)
+    login_frame.pack(pady=20)
+
+    tk.Label(login_frame, text="Email:").grid(column=0, row=0)
+    email_input = tk.Entry(login_frame)
+    email_input.grid(column=1, row=0)
+
+    tk.Label(login_frame, text="Password:").grid(column=0, row=1, pady=(10, 0))
+    pass_input = tk.Entry(login_frame, show="●")
+    pass_input.grid(column=1, row=1, pady=(10, 0))
+
+    remember_frame = tk.Frame(root)
+    remember_frame.pack()
+
+    remember_values = [tk.IntVar(), tk.IntVar()]
+    remember_email = tk.Checkbutton(remember_frame, text="Remember Email", variable=remember_values[0])
+    remember_email.pack(side="top", anchor="w")
+    remember_pass = tk.Checkbutton(remember_frame, text="Remember Password", variable=remember_values[1])
+    remember_pass.pack(side="top", anchor="w")
+
+    email_input.insert(0, data[0].replace("\n", ""))
+    pass_input.insert(0, data[1].replace("\n", ""))
+    if data[0] != "":
+        remember_email.select()
+    if data[1] != "":
+        remember_pass.select()
+    save.close()
+
+    tk.Button(root, text="Done", command=done).pack(pady=20)
+
+    root.mainloop()
+    return output
+
+if __name__ == "__main__":
+    login_ui()
--- main.py
+++ main.py
@@ -8,11 +8,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 
+
 from time import sleep
 from random import randint
 import pyautogui
 from win10toast import ToastNotifier
 from playsound import playsound
+import traceback
+from login_ui import login_ui
 
 
 
@@ -22,12 +25,14 @@ from playsound import playsound
 # ---- EMAIL, MDP, LISTE ANIMAUX ----
 # ____________________________________
 
-email = "exemple@exemple.com"
-mdp = "1234"
+
+email, mdp = login_ui()
 
 
 liste_recherches = ['Canida', 'Felida', 'Ca', 'Cattl', 'Do', 'Donke', 'Goa', 'Guinea pi', 'Hors', 'Pi', 'Rabbi', 'Fancy rat varietie', 'laboratory rat strain', 'Sheep breed', 'Water buffalo breed', 'Chicken breed', 'Duck breed', 'Goose breed', 'Pigeon breed', 'Turkey breed', 'Aardvar', 'Aardwol', 'African buffal', 'African elephan', 'African leopar', 'Albatros', 'Alligato', 'Alpac', 'American buffalo (bison', 'American robi', 'Amphibia', 'lis', 'Anacond', 'Angelfis', 'Anglerfis', 'An', 'Anteate', 'Antelop', 'Antlio', 'Ap', 'Aphi', 'Arabian leopar', 'Arctic Fo', 'Arctic Wol', 'Armadill', 'Arrow cra', 'As', 'Ass (donkey', 'Baboo', 'Badge', 'Bald eagl', 'Bandicoo', 'Barnacl', 'Barracud', 'Basilis', 'Bas', 'Ba', 'Beaked whal', 'Bea', 'lis', 'Beave', 'Bedbu', 'Be', 'Beetl', 'Bir', 'lis', 'Biso', 'Blackbir', 'Black panthe', 'Black widow spide', 'Blue bir', 'Blue ja', 'Blue whal', 'Bo', 'Boa', 'Bobca', 'Bobolin', 'Bonob', 'Boob', 'Box jellyfis', 'Bovi', 'Buffalo, Africa', 'Buffalo, American (bison', 'Bu', 'Butterfl', 'Buzzar', 'Came', 'Cani', 'Cape buffal', 'Capybar', 'Cardina', 'Caribo', 'Car', 'Ca', 'lis', 'Catshar', 'Caterpilla', 'Catfis', 'Cattl', 'lis', 'Centiped', 'Cephalopo', 'Chameleo', 'Cheeta', 'Chickade', 'Chicke', 'lis', 'Chimpanze', 'Chinchill', 'Chipmun', 'Cla', 'Clownfis', 'Cobr', 'Cockroac', 'Co', 'Condo', 'Constricto', 'Cora', 'Couga', 'Co', 'Coyot', 'Cra', 'Cran', 'Crane fl', 'Crawda', 'Crayfis', 'Cricke', 'Crocodil', 'Cro', 'Cucko', 'Cicad', 'Damselfl', 'Dee', 'Ding', 'Dinosau', 'lis', 'Do', 'lis', 'Dolphi', 'Donke', 'lis', 'Dormous', 'Dov', 'Dragonfl', 'Drago', 'Duc', 'lis', 'Dung beetl', 'Eagl', 'Earthwor', 'Earwi', 'Echidn', 'Ee', 'Egre', 'Elephan', 'Elephant sea', 'El', 'Em', 'English pointe', 'Ermin', 'Falco', 'Ferre', 'Finc', 'Firefl', 'Fis', 'Flaming', 'Fle', 'Fl', 'Flyingfis', 'Fow', 'Fo', 'Fro', 'Fruit ba', 'Gamefow', 'lis', 'Gallifor', 'lis', 'Gazell', 'Geck', 'Gerbi', 'Giant pand', 'Giant squi', 'Gibbo', 'Gila monste', 'Giraff', 'Goa', 'lis', 'Goldfis', 'Goos', 'lis', 'Gophe', 'Gorill', 'Grasshoppe', 'Great blue hero', 'Great white shar', 'Grizzly bea', 'Ground shar', 'Ground slot', 'Grous', 'Gua', 'lis', 'Guanac', 'Guineafow', 'lis', 'Guinea pi', 'lis', 'Gul', 'Gupp', 'Haddoc', 'Halibu', 'Hammerhead shar', 'Hamste', 'Har', 'Harrie', 'Haw', 'Hedgeho', 'Hermit cra', 'Hero', 'Herrin', 'Hippopotamu', 'Hookwor', 'Horne', 'Hors', 'lis', 'Hoverfl', 'Hummingbir', 'Humpback whal', 'Hyen', 'Iguan', 'Impal', 'Irukandji jellyfis', 'Jacka', 'Jagua', 'Ja', 'Jellyfis', 'Junglefow', 'Kangaro', 'Kangaroo mous', 'Kangaroo ra', 'Kingfishe', 'Kit', 'Kiw', 'Koal', 'Ko', 'Komodo drago', 'Kril', 'Ladybu', 'Lampre', 'Landfow', 'Land snai', 'Lar', 'Leec', 'Lemmin', 'Lemu', 'Leopar', 'Leopo', 'Limpe', 'Lio', 'Lizar', 'Llam', 'Lobste', 'Locus', 'Loo', 'Lous', 'Lungfis', 'Lyn', 'Maca', 'Mackere', 'Magpi', 'Mamma', 'Manate', 'Mandril', 'Manta ra', 'Marli', 'Marmose', 'Marmo', 'Marsupia', 'Marte', 'Mastodo', 'Meadowlar', 'Meerka', 'Min', 'Minno', 'Mit', 'Mockingbir', 'Mol', 'Mollus', 'Mongoos', 'Monitor lizar', 'Monke', 'Moos', 'Mosquit', 'Mot', 'Mountain goa', 'Mous', 'Mul', 'Musko', 'Narwha', 'New', 'New World quai', 'Nightingal', 'Ocelo', 'Octopu', 'Old World quai', 'Opossu', 'Oranguta', 'Orc', 'Ostric', 'Otte', 'Ow', 'O', 'Pand', 'Panthe', 'Panthera hybri', 'Parakee', 'Parro', 'Parrotfis', 'Partridg', 'Peacoc', 'Peafow', 'Pelica', 'Pengui', 'Perc', 'Peregrine falco', 'Pheasan', 'Pi', 'Pigeo', 'lis', 'Pik', 'Pilot whal', 'Pinnipe', 'Piranh', 'Planaria', 'Platypu', 'Polar bea', 'Pon', 'Porcupin', 'Porpois', "Portuguese man o' wa", 'Possu', 'Prairie do', 'Praw', 'Praying manti', 'Primat', 'Ptarmiga', 'Puffi', 'Pum', 'Pytho', 'Quai', 'Quele', 'Quokk', 'Rabbi', 'lis', 'Raccoo', 'Rainbow trou', 'Ra', 'Rattlesnak', 'Rave', 'Ray (Batoidea', 'Ray (Rajiformes', 'Red pand', 'Reindee', 'Reptil', 'Rhinocero', 'Right whal', 'Roadrunne', 'Roden', 'Roo', 'Rooste', 'Roundwor', 'Saber-toothed ca', 'Sailfis', 'Salamande', 'Salmo', 'Sawfis', 'Scale insec', 'Scallo', 'Scorpio', 'Seahors', 'Sea lio', 'Sea slu', 'Sea snai', 'Shar', 'lis', 'Shee', 'lis', 'Shre', 'Shrim', 'Silkwor', 'Silverfis', 'Skin', 'Skun', 'Slot', 'Slu', 'Smel', 'Snai', 'Snak', 'lis', 'Snip', 'Snow leopar', 'Sockeye salmo', 'Sol', 'Sparro', 'Sperm whal', 'Spide', 'Spider monke', 'Spoonbil', 'Squi', 'Squirre', 'Starfis', 'Star-nosed mol', 'Steelhead trou', 'Stingra', 'Stoa', 'Stor', 'Sturgeo', 'Sugar glide', 'Swallo', 'Swa', 'Swif', 'Swordfis', 'Swordtai', 'Tah', 'Taki', 'Tapi', 'Tarantul', 'Tarsie', 'Tasmanian devi', 'Termit', 'Ter', 'Thrus', 'Tic', 'Tige', 'Tiger shar', 'Tiglo', 'Toa', 'Tortois', 'Touca', 'Trapdoor spide', 'Tree fro', 'Trou', 'Tun', 'Turke', 'lis', 'Turtl', 'Tyrannosauru', 'Uria', 'Vampire ba', 'Vampire squi', 'Vicun', 'Vipe', 'Vol', 'Vultur', 'Wallab', 'Walru', 'Was', 'Warble', 'Water Bo', 'Water buffal', 'Wease', 'Whal', 'Whippe', 'Whitefis', 'Whooping cran', 'Wildca', 'Wildebees', 'Wildfow', 'Wol', 'Wolverin', 'Womba', 'Woodpecke', 'Wor', 'Wre', 'Xerina', 'X-ray fis', 'Ya', 'Yellow perc', 'Zebr', 'Zebra finc', 'Animals by number of neuron', 'Animals by siz', 'Common household pest', 'Common names of poisonous animal', 'Alpac', 'Bali cattl', 'Ca', 'Cattl', 'Chicke', 'Do', 'Domestic Bactrian came', 'Domestic canar', 'Domestic dromedary came', 'Domestic duc', 'Domestic goa', 'Domestic goos', 'Domestic guineafow', 'Domestic hedgeho', 'Domestic pi', 'Domestic pigeo', 'Domestic rabbi', 'Domestic silkmot', 'Domestic silver fo', 'Domestic turke', 'Donke', 'Fancy mous', 'Fancy ra', 'Lab ra', 'Ferre', 'Gaya', 'Goldfis', 'Guinea pi', 'Gupp', 'Hors', 'Ko', 'Llam', 'Ringneck dov', 'Shee', 'Siamese fighting fis', 'Society finc', 'Ya']
 
+
+driver = webdriver.Chrome(executable_path='chromedriver.exe')
 
 
 
@@ -83,7 +88,24 @@ def changer_url(url:str):
     pyautogui.press('enter')
 # ________________________________________________________
 
+def mobile(enabled=True):
+    if enabled:
+        set_device_metrics_override = dict({
+            "width": 375,
+            "height": 812,
+            "deviceScaleFactor": 50,
+            "mobile": True
+        })
+    else:
+        set_device_metrics_override = dict({
+            "width": 0,
+            "height": 0,
+            "deviceScaleFactor": 0,
+            "mobile": False
+        })
+    driver.execute_cdp_cmd('Emulation.setDeviceMetricsOverride', set_device_metrics_override)
 
+# ________________________________________________________
 
 # Effectue les recherches pc et téléphone
 
@@ -92,23 +114,18 @@ def recherches(nb_pc:int=40, nb_tel:int=30, liste_recherches:list=['banane', 'ou
 
         # Recherches PC
     for pc in range(nb_pc):
-        raccourci(['ctrl', 'l'])
-        pyautogui.press('backspace')
-        pyautogui.write(f'bing.com/search?q={liste_recherches[randint(0, len(liste_recherches)-1)]}')
-        pyautogui.press('enter')
+        driver.get(f'https://bing.com/search?q={liste_recherches[randint(0, len(liste_recherches)-1)]}')
         pause_pour_veski_le_bot(tps_en_moins=2)
 
         # Recherches Téléphone
-    raccourci(['ctrl', 'shift', 'i'])
-    raccourci(['ctrl', 'shift', 'm'])
-    for tel in range(nb_tel):
-        raccourci(['ctrl', 'l'])
-        pyautogui.press('backspace')
-        pyautogui.write(f'bing.com/search?q={liste_recherches[randint(0, len(liste_recherches)-1)]}')
-        pyautogui.press('enter')
-        pause_pour_veski_le_bot(tps_en_moins=2)
+    if nb_tel > 0:
+        mobile()
 
-    raccourci(['ctrl', 'shift', 'i'])
+        for tel in range(nb_tel):
+            driver.get(f'https://bing.com/search?q={liste_recherches[randint(0, len(liste_recherches)-1)]}')
+            pause_pour_veski_le_bot(tps_en_moins=2)
+
+        mobile(False)
 # ________________________________________________________
 
 
@@ -124,35 +141,49 @@ def rewards_auto_daily_weekly(set):
                 truc['link'].click()
                 pause_pour_veski_le_bot()
                 pause_pour_veski_le_bot()
-                raccourci(['ctrl', 'w'])
+                driver.switch_to.window(driver.window_handles[-1])
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
 
             elif truc['type'] == 'sondage':
                 truc['link'].click()
                 pause_pour_veski_le_bot()
                 pause_pour_veski_le_bot()
+                driver.switch_to.window(driver.window_handles[-1])
                 if randint(1, 2) == 1:
-                    pyautogui.click(1280, 870)
+                    driver.find_element(By.ID, "btoption0").click()
                 else:
-                    pyautogui.click(1280, 950)
+                    driver.find_element(By.ID, "btoption1").click()
                 pause_pour_veski_le_bot()
                 pause_pour_veski_le_bot()
-                raccourci(['ctrl', 'w'])
-            
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
+
             elif truc['type'] == 'quiz':
                 truc['link'].click()
                 pause_pour_veski_le_bot()
-                pyautogui.click(274, 935)
+                driver.switch_to.window(driver.window_handles[-1])
+                driver.find_element(By.ID, "rqStartQuiz").click()
                 sleep(1)
                 for nb in range(3):
-                    for nb_res in range(8):
-                        pyautogui.click(221+128*nb_res, 926)
+                    answers = [e.get_attribute("id")
+                               for e in driver.find_elements(By.XPATH, '//*[@iscorrectoption="True"]')]
+                    print(driver.find_elements(By.XPATH, '//*[@iscorrectoption="True"]'))
+                    for a in answers:
+                        driver.find_element(By.ID, a).click()
                         pause_pour_veski_le_bot(tps_en_moins=2)
+                        # Si le nb de crédit augmente, on a la bonne de réponse donc on arrete de cliquer sur les
+                        # réponses
+                        if int(driver.find_element(By.CLASS_NAME, "rqECredits").text) == nb*10 + 10:
+                            break
                     pause_pour_veski_le_bot()
-                raccourci(['ctrl', 'w'])
-            
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
+
             elif truc['type'] == 'ceci_cela_quiz':
                 truc['link'].click()
                 pause_pour_veski_le_bot()
+                driver.switch_to.window(driver.window_handles[-1])
                 pyautogui.click(274, 935)
                 sleep(1)
                 for question in range(10):
@@ -161,7 +192,8 @@ def rewards_auto_daily_weekly(set):
                     else:
                         pyautogui.click(1150, 840)
                     pause_pour_veski_le_bot()
-                raccourci(['ctrl', 'w'])
+                driver.close()
+                driver.switch_to.window(driver.window_handles[0])
 # ________________________________________________________
 
 
@@ -188,7 +220,7 @@ def pause_pour_veski_le_bot(t1=4, t2=7, tps_en_moins=0):
 def code_de_gaming(email, mdp, liste_recherches):
 
     try:
-        
+
         # Vérifie si un élément existe
         def check_element(type:str, element:str):
             try:
@@ -196,20 +228,19 @@ def code_de_gaming(email, mdp, liste_recherches):
                     driver.find_element(By.XPATH, element)
                 if type == 'By.ID':
                     driver.find_element(By.ID, element)
-                    
+
                 if element == 'bnp_btn_accept':
                     print('oui')
             except NoSuchElementException:
                 return False
             return True
-        
+
 
         #            ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
         #            ----- OUVERTURE DU NAVIGATEUR -----
         #            ____________________________________
 
 
-        driver = webdriver.Chrome(executable_path='chromedriver.exe')
         driver.get("https://bing.com")
         driver.maximize_window()
 
@@ -237,14 +268,14 @@ def code_de_gaming(email, mdp, liste_recherches):
 
         pause_pour_veski_le_bot()
 
-        
+
         # Compte perso (Si on demande le choix entre compte perso et compte pro, le compte perso est prioritaire)
         if check_element('By.XPATH', '//*[@id="msaTile"]/div') == True:
             compte_perso = driver.find_element(By.XPATH, '//*[@id="msaTile"]/div')
             compte_perso.click()
 
             pause_pour_veski_le_bot()
-        
+
 
         # Mot de passe
         mdp_input = driver.find_element(By.NAME, "passwd")
@@ -288,15 +319,15 @@ def code_de_gaming(email, mdp, liste_recherches):
 
 
 
-        changer_url('rewards.bing.com')
+        driver.get('https://rewards.bing.com')
 
         pause_pour_veski_le_bot()
 
         daily_set = []
 
         for e in range(3):
-            daily_set.append({  
-                'link' : driver.find_element(By.XPATH, f'//*[@id="daily-sets"]/mee-card-group[1]/div/mee-card[{e+1}]'), 
+            daily_set.append({
+                'link' : driver.find_element(By.XPATH, f'//*[@id="daily-sets"]/mee-card-group[1]/div/mee-card[{e+1}]'),
                 'check': is_check(driver.find_element(By.XPATH, f'//*[@id="daily-sets"]/mee-card-group[1]/div/mee-card[{e+1}]/div/card-content/mee-rewards-daily-set-item-content/div/a/mee-rewards-points/div/div/span[1]').get_attribute('class')),
                 'pts'  : driver.find_element(By.XPATH, f'//*[@id="daily-sets"]/mee-card-group[1]/div/mee-card[{e+1}]/div/card-content/mee-rewards-daily-set-item-content/div/a/mee-rewards-points/div/div/span[2]').text,
                 'type' : what_type(driver.find_element(By.XPATH, f'//*[@id="daily-sets"]/mee-card-group[1]/div/mee-card[{e+1}]/div/card-content/mee-rewards-daily-set-item-content/div/a/div[2]/h3').text)
@@ -324,7 +355,7 @@ def code_de_gaming(email, mdp, liste_recherches):
         'check': is_check(driver.find_element(By.XPATH, f'//*[@id="more-activities"]/div/mee-card[{p+1}]/div/card-content/mee-rewards-more-activities-card-item/div/a/mee-rewards-points/div/div/span[1]').get_attribute('class')),
         'pts'  : driver.find_element(By.XPATH, f'//*[@id="more-activities"]/div/mee-card[{p+1}]/div/card-content/mee-rewards-more-activities-card-item/div/a/mee-rewards-points/div/div/span[2]').text,
         'type' : what_type(driver.find_element(By.XPATH, f'//*[@id="more-activities"]/div/mee-card[{p+1}]/div/card-content/mee-rewards-more-activities-card-item/div/a/div[2]/h3').text),
-        'link' : driver.find_element(By.XPATH, f'//*[@id="more-activities"]/div/mee-card[{p+1}]')    
+        'link' : driver.find_element(By.XPATH, f'//*[@id="more-activities"]/div/mee-card[{p+1}]')
                 })
             except:
                 print(f"L'itération {p+1} n'est pas un truc qui rapporte des points")
@@ -343,13 +374,16 @@ def code_de_gaming(email, mdp, liste_recherches):
         playsound(sound='notif.mp3')
         ToastNotifier().show_toast(title="Microsoft Rewards", msg="Toutes les tâches ont été effectuées", duration=10)
 
-    
 
-    except:
+
+    except Exception as e:
 
         playsound(sound='notif.mp3')
         ToastNotifier().show_toast(title="Microsoft Rewards", msg="Erreur", duration=10)
-    
+
+        print(e)
+        print(traceback.print_exc())
+
 #            ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
 #            ------- FAIRE TOUS LES TRUCS -------
 #            ____________________________________
@@ -379,4 +413,3 @@ exit()'''
 
 
 
-        
